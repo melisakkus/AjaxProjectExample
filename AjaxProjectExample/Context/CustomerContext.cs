b1213698a40c68f2ea4bdbe0406e15b0ec10eb94
Melisa Akkuş <melisa.akkus01@gmail.com>
2025-02-21 22:00:39 +0300

    Add project files.

---
+++ AjaxProjectExample/Context/CustomerContext.cs
@@ -0,0 +1,14 @@
+﻿using AjaxProjectExample.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace AjaxProjectExample.Context
+{
+    public class CustomerContext : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=DESKTOP-B6FJQDJ\\SQLEXPRESS;Initial Catalog=CustomerAjaxDb;Integrated Security=True;TrustServerCertificate=True;");
+        }
+        public DbSet<Customer> Customers { get; set; }
+    }
+}
